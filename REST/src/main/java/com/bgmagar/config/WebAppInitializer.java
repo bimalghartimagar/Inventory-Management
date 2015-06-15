a52b6f6f117cfbcfbf192e5bc7ddbbb338c5f8d4
bimalghartimagar <i81162@intern4.d2hs.com>
2015-06-15 17:28:03 +0545

    web app changes

--- REST/src/main/java/com/bgmagar/config/WebAppInitializer.java
+++ REST/src/main/java/com/bgmagar/config/WebAppInitializer.java
@@ -1,6 +1,5 @@
 package com.bgmagar.config;
 
-import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration.Dynamic;
